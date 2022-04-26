7e7bbf8b761c1d2326295483a958aad55bd847f6
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-04-26 10:42:10 +0200

    First draft  of asynchronous server discovery => Current Issues we online get http address and not websocket

---
+++ Assets/Package/Runtime/ServerDiscovery.cs
@@ -0,0 +1,52 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Unity.RenderStreaming;
+using Unity.RenderStreaming.Signaling;
+using UnityEngine;
+
+public class ServerDiscovery : MonoBehaviour {
+    private volatile string serverAddress;
+    private volatile bool serverDiscovered;
+    private bool serverSetup;
+    
+    void Start() {
+        var portMessage = new Thread(ScanPortInSystem) {
+            IsBackground = true
+        };
+        portMessage.Start();
+    }
+
+    private void Update() {
+        if(!serverSetup && serverDiscovered) {
+            string sanitizedAddress = serverAddress.Split(':')[0];
+            ISignaling signaling = new WebSocketSignaling($"http://{sanitizedAddress}:80", 5.0f, SynchronizationContext.Current);
+            SignalingHandlerBase handlerBase = GetComponent<Broadcast>();
+            GetComponent<RenderStreaming>().Run(true, signaling, new []{handlerBase});
+        }
+    }
+
+    private async Task AsyncSearchForServer() {
+        Thread portMessage = new Thread(ScanPortInSystem);
+        portMessage.Start();
+        portMessage.Join();
+    }
+
+    private void ScanPortInSystem() {
+        Socket sock = new Socket(AddressFamily.InterNetwork,
+            SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint iep = new IPEndPoint(IPAddress.Any, 41234);
+        sock.Bind(iep);
+        EndPoint ep = iep;
+        Debug.Log("Waiting for streaming server");
+        byte[] data = new byte[1024];
+        int receivedDate = sock.ReceiveFrom(data, ref ep);
+        string stringData = Encoding.ASCII.GetString(data, 0, receivedDate);
+        Debug.Log($"received: {stringData} from: {ep}");
+        sock.Close();
+        serverAddress = ep.ToString();
+        serverDiscovered = true;
+    }
+}
