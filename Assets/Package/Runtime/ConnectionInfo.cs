8574125022bd4ac1c4650dd3b4a7a2b06433d287
ChilledCosmonaut <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-05-30 16:17:47 +0200

    Created ConnectionInfo class as container for Json messages

---
+++ Assets/Package/Runtime/ConnectionInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Package.Runtime
+{
+    public class ConnectionInfo
+    {
+        public string ID;
+
+        public List<(string transmissionType, string ipAddress)> RequestedTransmissions;
+    }
+}
