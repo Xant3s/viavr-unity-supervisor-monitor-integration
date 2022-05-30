4372526cfb20b5004ed4c0ab931f4451bb7b5137
ChilledCosmonaut <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-05-30 19:51:18 +0200

    Fixed non-serializable Class

--- Assets/Package/Runtime/ConnectionInfo.cs
+++ Assets/Package/Runtime/ConnectionInfo.cs
@@ -1,11 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+// ReSharper disable ClassNeverInstantiated.Global
 
 namespace Package.Runtime
 {
+    [Serializable]
+    public class TransmissionInfo
+    {
+        public string Typ;
+
+        public string Ip;
+    }
+    
+    [Serializable]
     public class ConnectionInfo
     {
         public string ID;
 
-        public List<(string transmissionType, string ipAddress)> RequestedTransmissions = new();
+        public List<TransmissionInfo> RequestedTransmissions = new();
     }
 }
