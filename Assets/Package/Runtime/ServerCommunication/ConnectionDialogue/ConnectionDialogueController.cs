e30faa47d09b007a4b7e3fbebb7788321d0e8bb7
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-06-10 18:05:51 +0200

    Created logic for the prompt

--- Assets/Package/Runtime/ServerCommunication/ConnectionDialogue/ConnectionDialogueController.cs
+++ Assets/Package/Runtime/ServerCommunication/ConnectionDialogue/ConnectionDialogueController.cs
@@ -1,24 +1,32 @@
-﻿using UnityEngine.Events;
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Button = UnityEngine.UIElements.Button;
 
 namespace Package.Runtime.ServerCommunication.ConnectionDialogue {
-    public class ConnectionDialogueController {
+    public class ConnectionDialogueController : MonoBehaviour {
+        [SerializeField] private Text header;
+        [SerializeField] private Text body;
+        [SerializeField] private Button acceptButton;
+        [SerializeField] private Button declineButton;
+        
         private UnityAction onAccept;
         private UnityAction onDecline;
 
-        public ConnectionDialogueController() {
-            
-        }
-        
         public void Show(string id, string details) {
-            
+            header.text = $"Server ID: {id}";
+            body.text = $"Server requires:";
+            gameObject.SetActive(true);
         }
 
-        public void OnConnectionDeclined() {
-            
+        public void AddOnConnectionDeclined() {
+            gameObject.SetActive(false);
+            acceptButton.clicked += () => onAccept?.Invoke();
         }
 
-        public void OnConnectionAccepted() {
-            
+        public void AddOnConnectionAccepted() {
+            gameObject.SetActive(false);
+            declineButton.clicked += () => onDecline?.Invoke();
         }
     }
 }
