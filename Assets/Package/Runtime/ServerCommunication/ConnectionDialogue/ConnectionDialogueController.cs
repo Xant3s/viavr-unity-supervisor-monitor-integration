0f0a92b48d4de07d3857014dfcd75ef8cf42f73b
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-06-10 17:18:08 +0200

    Set up base structure for Prompt class

---
+++ Assets/Package/Runtime/ServerCommunication/ConnectionDialogue/ConnectionDialogueController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.Events;
+
+namespace Package.Runtime.ServerCommunication.ConnectionDialogue {
+    public class ConnectionDialogueController {
+        private UnityAction onAccept;
+        private UnityAction onDecline;
+
+        public ConnectionDialogueController() {
+            
+        }
+        
+        public void Show(string id, string details) {
+            
+        }
+
+        public void OnConnectionDeclined() {
+            
+        }
+
+        public void OnConnectionAccepted() {
+            
+        }
+    }
+}
