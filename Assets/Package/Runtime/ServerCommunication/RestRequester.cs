9dfe0c2f163802425729495cae6d64d80d9a6285
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-06-22 18:36:01 +0200

    Created GetRequest that also provides a callback for asynchronous operations

---
+++ Assets/Package/Runtime/ServerCommunication/RestRequester.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Package.Runtime.Communication {
+    public class RestRequester : MonoBehaviour{
+
+        private void Start() {
+            MakeGetRequest("", text => Debug.Log("nReceived: " + text));
+        }
+
+        private const string ipAddress = "localhost:3000/";
+
+        private static void MakeGetRequest(string identifier, Action<string> onReception) {
+            UnityWebRequest webRequest = UnityWebRequest.Get(ipAddress + identifier);
+
+            // Request and wait for the desired page.
+            UnityWebRequestAsyncOperation requestAsyncOperation = webRequest.SendWebRequest();
+
+            requestAsyncOperation.completed += operation => {
+                switch(webRequest.result) {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                        Debug.LogError("Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.ProtocolError:
+                        Debug.LogError("HTTP Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        onReception(webRequest.downloadHandler.text);
+                        break;
+                }
+                webRequest.Dispose();
+            };
+        }
+    }
+}
