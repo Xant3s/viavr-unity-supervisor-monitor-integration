eaff1c18148924d42a869007e73cd4a13952e7b3
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-06-22 18:43:57 +0200

    Provided other methods to make Post and Put requests

--- Assets/Package/Runtime/ServerCommunication/RestRequester.cs
+++ Assets/Package/Runtime/ServerCommunication/RestRequester.cs
@@ -9,10 +9,58 @@ namespace Package.Runtime.Communication {
             MakeGetRequest("", text => Debug.Log("nReceived: " + text));
         }
 
-        private const string ipAddress = "localhost:3000/";
+        private const string IPAddress = "localhost:3000/";
 
-        private static void MakeGetRequest(string identifier, Action<string> onReception) {
-            UnityWebRequest webRequest = UnityWebRequest.Get(ipAddress + identifier);
+        public static void MakeGetRequest(string identifier, Action<string> onReception) {
+            UnityWebRequest webRequest = UnityWebRequest.Get(IPAddress + identifier);
+
+            // Request and wait for the desired page.
+            UnityWebRequestAsyncOperation requestAsyncOperation = webRequest.SendWebRequest();
+
+            requestAsyncOperation.completed += operation => {
+                switch(webRequest.result) {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                        Debug.LogError("Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.ProtocolError:
+                        Debug.LogError("HTTP Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        onReception(webRequest.downloadHandler.text);
+                        break;
+                }
+                webRequest.Dispose();
+            };
+        }
+        
+        public static void MakePostRequest(string identifier, Action<string> onReception) {
+            WWWForm form = new WWWForm();
+            UnityWebRequest webRequest = UnityWebRequest.Post(IPAddress + identifier, form);
+
+            // Request and wait for the desired page.
+            UnityWebRequestAsyncOperation requestAsyncOperation = webRequest.SendWebRequest();
+
+            requestAsyncOperation.completed += operation => {
+                switch(webRequest.result) {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                        Debug.LogError("Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.ProtocolError:
+                        Debug.LogError("HTTP Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        onReception(webRequest.downloadHandler.text);
+                        break;
+                }
+                webRequest.Dispose();
+            };
+        }
+        
+        public static void MakePutRequest(string identifier, Action<string> onReception) {
+            byte[] sampleData = System.Text.Encoding.UTF8.GetBytes("Test Bytes");
+            UnityWebRequest webRequest = UnityWebRequest.Put(IPAddress + identifier, sampleData);
 
             // Request and wait for the desired page.
             UnityWebRequestAsyncOperation requestAsyncOperation = webRequest.SendWebRequest();
