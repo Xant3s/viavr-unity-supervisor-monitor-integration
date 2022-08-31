bfbf3c51ca8b975e851d75d4bb43bdb88b6389ed
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-08-31 15:05:32 +0200

    Basic logic for events initialising custom Tasks with conditions coupled to game-states and custom message inference

--- Packages/de.jmu.ge.viavr.webstreaming/Runtime/EventSystem/Event.cs
+++ Packages/de.jmu.ge.viavr.webstreaming/Runtime/EventSystem/Event.cs
@@ -1,23 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 using de.jmu.ge.Gamification.General;
 using Package.Runtime.Communication;
+using GamesEngineering.QuestSystem;
 
 namespace EventSystem {
-    public class Event {
-        private readonly string identifier;
-        private readonly GameState trackedState;
-        
-        public Event(string identifier, GameState trackedState) {
-            this.identifier = identifier;
-            this.trackedState = trackedState;
+    public class GameStateRelation {
+        private string name;
+        private GameState gameState;
+        private bool relation;
+        private int targetValue;
+
+        public GameStateRelation(string gameStateName, bool relation, int targetValue) {
+            name = gameStateName;
+            this.relation = relation;
+            this.targetValue = targetValue;
         }
 
-        public void Invoke() => SendToSupervisor(trackedState.FormatGameStateInfo());
+        public void SetCorrespondingGameState(GameState correspondingState) => gameState = correspondingState;
+    }
+    
+    public class Event {
+        private const string Identifier = "LogEvent";
+        
+        private readonly List<(string name, Relation relation, int targetValue)> gameStateInfo;
+        private readonly Func<string> onActivateMessage;
+        private readonly bool repeated;
 
-        private void SendToSupervisor(string eventMessage) => 
+        private string mappedGameStates;
+        private Task currentTask;
+
+        public Event(List<(string name, Relation relation, int targetValue)> gameStateInfo, string onActionMessage, bool repeated) {
+            this.gameStateInfo = gameStateInfo;
+            var messageParts = onActionMessage.Split('{','}');
+            onActivateMessage += () => string.Concat(messageParts);
+            this.repeated = repeated;
+        }
+
+        public void InitialiseTask(Dictionary<string, GameState> mappedGameStates) {
+            this.mappedGameStates = this.mappedGameStates;
+            currentTask = new Task();
+            
+            foreach(var info in gameStateInfo) {
+                var currentCondition = new Condition();
+                GameState.onValueChange += () => 
+                    currentCondition.IsTrue = int.Parse(mappedGameStates[info.name].Value) >= info.targetValue;
+                currentTask.Conditions.Add(currentCondition);
+            }
+            
+            currentTask.OnTaskFinished.AddListener(() => {
+                SendToSupervisor();
+                currentTask.IsActive = repeated;
+            });
+
+            currentTask.IsActive = true;
+        }
+
+        private void SendToSupervisor() => 
             RestRequester.GetInstance().MakePutRequest(
-                identifier,
+                Identifier,
                 _ => {},
-                string.Join(" ", DateTime.Now.ToShortTimeString(), eventMessage));
+                string.Join(" ", "[" + DateTime.Now.ToShortTimeString() + "]", onActivateMessage()));
     }
 }
---
+++ Packages/de.jmu.ge.viavr.webstreaming/Runtime/EventSystem/EventConfigurator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using WebStreaming;
+
+namespace EventSystem {
+    public enum Relation {
+        Lesser,
+        Equal,
+        Greater
+    }
+    public class EventConfigurator : MonoBehaviour {
+        private List<Event> events = new();
+        private List<(string name, Relation relation, int targetValue)> gameStateInfo;
+
+        public void SetUpEvents(List<EventSettings> eventInfo) {
+            foreach(var trackedEvent in eventInfo) {
+                events.Add(new Event(ConvertGameStateInfo(trackedEvent.trackedGameStates),
+                    trackedEvent.onActivateMessage,
+                    bool.Parse(trackedEvent.repeated)));
+            }
+        }
+
+        private List<(string name, Relation relation, int targetValue)> ConvertGameStateInfo(List<GameStateSettings> gameStateInfos) =>
+            gameStateInfos.Select(info => (info.gameStateName, Enum.Parse<Relation>(info.relation), int.Parse(info.targetValue)))
+                .ToList();
+    }
+}
