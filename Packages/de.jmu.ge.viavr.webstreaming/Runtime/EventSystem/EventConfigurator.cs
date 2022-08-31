bde928a693d3771c3b8323adec381ab81852a63f
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-08-31 15:41:32 +0200

    EventConfigurator now initialises events on start

--- Packages/de.jmu.ge.viavr.webstreaming/Runtime/EventSystem/EventConfigurator.cs
+++ Packages/de.jmu.ge.viavr.webstreaming/Runtime/EventSystem/EventConfigurator.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using de.jmu.ge.Gamification.General;
+using UnityEditor;
 using UnityEngine;
 using WebStreaming;
 
@@ -11,6 +13,8 @@ namespace EventSystem {
         Greater
     }
     public class EventConfigurator : MonoBehaviour {
+        //Copied from GamificationConfigurator. Sadly cannot reference it directly because it is Editor only
+        private const string path = "Assets/Settings/de.jmu.ge.gamificationutils/States/";
         private List<Event> events = new();
         private List<(string name, Relation relation, int targetValue)> gameStateInfo;
 
@@ -20,6 +24,26 @@ namespace EventSystem {
                     trackedEvent.onActivateMessage,
                     bool.Parse(trackedEvent.repeated)));
             }
+        }
+
+        private void Start() {
+            var gameStates = FindAllExistingGameStates();
+            var mappedGameStates = MapGameStates(gameStates);
+
+            foreach(var @event in events) {
+                @event.InitialiseTask(mappedGameStates);
+            }
+        }
+
+        private Dictionary<string, GameState> MapGameStates(List<GameState> gameStates)
+            => gameStates.ToDictionary(state => state.name);
+        
+        private List<GameState> FindAllExistingGameStates() {
+            var folders = new[] {path};
+            var findAssets = AssetDatabase.FindAssets("t: GameState", folders);
+
+            return findAssets.Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<GameState>).ToList();
         }
 
         private List<(string name, Relation relation, int targetValue)> ConvertGameStateInfo(List<GameStateSettings> gameStateInfos) =>
