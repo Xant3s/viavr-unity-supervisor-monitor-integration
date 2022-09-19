3e353a6431b4b8c4549067d78f2ed3f69eb8a7aa
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-09-19 15:32:10 +0200

    Send post request to register client

---
+++ Packages/de.jmu.ge.viavr.webstreaming/Runtime/RegisterClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace de.jmu.ge.viavr.webstreaming {
+    public class RegisterClient: MonoBehaviour {
+        public async void Register(IPEndPoint supervisorEndPoint) {
+            Debug.Log("Registering client at " + supervisorEndPoint);
+            var content = "{\"platform\": \"Android\", \"ID\": \"1\"}";
+            var data = new StringContent(content, Encoding.UTF8, "application/json");
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri($"http://{supervisorEndPoint.Address}:3001");
+            var response = await client.PostAsync("/clients/register", data);
+            var result = response.Content.ReadAsStringAsync().Result;
+            Debug.Log(result);
+        }
+    }
+}
--- Packages/de.jmu.ge.viavr.webstreaming/Runtime/SupervisorDiscovery.cs
+++ Packages/de.jmu.ge.viavr.webstreaming/Runtime/SupervisorDiscovery.cs
@@ -1,15 +1,20 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace de.jmu.ge.viavr.webstreaming {
     public class SupervisorDiscovery : MonoBehaviour {
+        public UnityEvent<IPEndPoint> onSupervisorFound;
         private const string connectionMessage = "Supervisor monitor looking for client";
         private Socket supervisorSocket;
         private Thread listenThread;
-
+        private IPEndPoint supervisorEndPoint;
+        private bool first;
+        
 
         private void Start() {
             supervisorSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
@@ -31,8 +36,16 @@ namespace de.jmu.ge.viavr.webstreaming {
                 messageData = Encoding.ASCII.GetString(data, 0, receivedDate);
                 Debug.Log($"received: {messageData} from {endPoint}");
             } while (!messageData.Contains(connectionMessage));
-            var supervisorEndPoint = (IPEndPoint) endPoint;
+            supervisorEndPoint = (IPEndPoint) endPoint;
             Debug.Log($"Supervisor found at {supervisorEndPoint.Address}:{supervisorEndPoint.Port}");
+            // onSupervisorFound?.Invoke(supervisorEndPoint);
+        }
+
+        private void Update() {
+            if(supervisorEndPoint != null && !first) {
+                onSupervisorFound?.Invoke(supervisorEndPoint);
+                first = true;
+            }
         }
 
         public void Stop() {
