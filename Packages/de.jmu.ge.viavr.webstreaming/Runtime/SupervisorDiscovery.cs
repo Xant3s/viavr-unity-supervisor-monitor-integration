1ea793c8474b946706f0fc52dc7d1f802a20e8e8
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-09-26 17:09:53 +0200

    Wait for connection request

--- Packages/de.jmu.ge.viavr.webstreaming/Runtime/SupervisorDiscovery.cs
+++ Packages/de.jmu.ge.viavr.webstreaming/Runtime/SupervisorDiscovery.cs
@@ -2,40 +2,43 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace de.jmu.ge.viavr.webstreaming {
     public class SupervisorDiscovery {
         public event EventHandler<IPEndPoint> OnSupervisorFound;
         private const string connectionMessage = "Supervisor monitor looking for client";
         private const int port = 41234;
-        private Socket supervisorSocket;
-        private Thread listenThread;
+        private readonly Timer timer = new();
+        private Socket socket;
+        private EndPoint endPoint;
 
 
         public void Start() {
-            supervisorSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            var endPoint = new IPEndPoint(IPAddress.Any, port);
-            supervisorSocket.Bind(endPoint);
-            listenThread = new Thread(() => ListenForSupervisor(supervisorSocket, endPoint)) {
-                IsBackground = true
-            };
-            listenThread.Start();
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            endPoint = new IPEndPoint(IPAddress.Any, port);
+            socket.Bind(endPoint);
+            timer.Interval = 500;
+            timer.Elapsed += ListenForSupervisor;
+            timer.Start();
         }
 
         public void Stop() {
-            listenThread?.Abort();
-            supervisorSocket?.Close();
+            timer.Stop();
+            socket?.Close();
         }
 
-        private void ListenForSupervisor(Socket socket, EndPoint endPoint) {
-            string messageData;
-            do {
-                var data = new byte[1024];
-                var receivedData = socket.ReceiveFrom(data, ref endPoint);
-                messageData = Encoding.ASCII.GetString(data, 0, receivedData);
-            } while (!messageData.Contains(connectionMessage));
+        private void ListenForSupervisor(object sender, ElapsedEventArgs e) => ListenForSupervisor();
+
+        private void ListenForSupervisor() {
+            var data = new byte[1024];
+            var receivedData = socket.ReceiveFrom(data, ref endPoint);
+            var messageData = Encoding.ASCII.GetString(data, 0, receivedData);
+            if(!messageData.Contains(connectionMessage)) return;
             OnSupervisorFound?.Invoke(this, (IPEndPoint) endPoint);
         }
+        
+        ~SupervisorDiscovery() => Stop();
     }
 }
--- Packages/de.jmu.ge.viavr.webstreaming/Runtime/SupervisorManager.cs
+++ Packages/de.jmu.ge.viavr.webstreaming/Runtime/SupervisorManager.cs
@@ -7,9 +7,12 @@ namespace de.jmu.ge.viavr.webstreaming {
     /// </summary>
     public class SupervisorManager: MonoBehaviour {
         private readonly SupervisorDiscovery discovery = new();
+        private WaitForConnectionRequest connection;
+        private const int restPort = 3001;
         private string deviceName;
         private string operatingSystem;
-
+        private string baseAddress;
+        
 
         private void Awake() {
             deviceName = SystemInfo.deviceName;
@@ -17,14 +20,33 @@ namespace de.jmu.ge.viavr.webstreaming {
         }
 
         private void Start() {
-            Debug.Log("Looking for supervisor");
             discovery.OnSupervisorFound += RegisterClient;
+            discovery.OnSupervisorFound += WaitForConnectionRequest;
+            discovery.OnSupervisorFound += StopDiscovery;
             discovery.Start();
         }
 
         private void RegisterClient(object sender, IPEndPoint endPoint) {
             Debug.Log($"Supervisor found at {endPoint.Address}:{endPoint.Port}");
-            new RegisterClient().Register(endPoint, deviceName, operatingSystem);
+            var supervisor = new IPEndPoint(endPoint.Address, restPort);
+            new RegisterClient().Register(supervisor, deviceName, operatingSystem);
+        }
+
+        private void WaitForConnectionRequest(object sender, IPEndPoint endPoint) {
+            Debug.Log("Start waiting for supervisor connection request.");
+            connection = new WaitForConnectionRequest($"http://{endPoint.Address}:{restPort}", deviceName);
+            connection.OnConnectionDiscarded += (_, _) => Debug.Log("discarded");
+            connection.OnConnectionRequested += (_, _) => Debug.Log("requested");     
+            connection.Start();
+        }
+
+        private void StopDiscovery(object _, IPEndPoint __) {
+            discovery.Stop();
+        }
+        
+        private void OnDestroy() {
+            discovery.Stop();
+            connection?.Stop();
         }
     }
 }
