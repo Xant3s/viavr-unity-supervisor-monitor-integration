6f42475db7e55bc81fd2294ffa01a511327ac02b
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-10-05 19:02:32 +0200

    Added event polling sample

--- Packages/de.jmu.ge.viavr.supervisorintegration/Samples~/Event Polling Example/PrintEvent.cs
+++ Packages/de.jmu.ge.viavr.supervisorintegration/Samples~/Event Polling Example/PrintEvent.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using de.jmu.ge.viavr.supervisorintegration;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace DefaultNamespace {
-    public class Test : MonoBehaviour {
+namespace de.jmu.ge.viavr.supervisorintegration {
+    public class PrintEvent : MonoBehaviour {
         private void Start() {
             var manager = FindObjectOfType<SupervisorManager>();
             manager.EventPoller.On("test1", e => {
