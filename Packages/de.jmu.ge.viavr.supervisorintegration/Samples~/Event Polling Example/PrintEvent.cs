ccc0f10322effdfa5d762f069b197c24e933e918
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-10-05 19:26:30 +0200

    Added note to event polling example

--- Packages/de.jmu.ge.viavr.supervisorintegration/Samples~/Event Polling Example/PrintEvent.cs
+++ Packages/de.jmu.ge.viavr.supervisorintegration/Samples~/Event Polling Example/PrintEvent.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 
 namespace de.jmu.ge.viavr.supervisorintegration {
+    // Requries the supervior to be running and adding events to the EventStorage.
+    // e.g. eventStorage.addEvent('test1', 42)
     public class PrintEvent : MonoBehaviour {
         private void Start() {
             var manager = FindObjectOfType<SupervisorManager>();
