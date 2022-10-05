76c9237b2d8ef5ef6c65ffe3f21c96c491732ccf
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2022-10-05 18:25:16 +0200

    RestRequester can now handle parameters in get requests

--- Packages/de.jmu.ge.viavr.supervisorintegration/Runtime/Util/RestRequester.cs
+++ Packages/de.jmu.ge.viavr.supervisorintegration/Runtime/Util/RestRequester.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,11 +15,28 @@ namespace de.jmu.ge.viavr.supervisorintegration {
             client.BaseAddress = new Uri(restServerBaseAddress);
         }
 
-        public async Task<string> Get(string path) => await client.GetStringAsync($"{restServerBaseAddress}{path}");
-
         public async Task<HttpResponseMessage> Post(string path, string content = null) {
             var data = content != null ? new StringContent(content, Encoding.UTF8, "application/json") : null;
             return await client.PostAsync($"{restServerBaseAddress}{path}", data);
+        }
+
+        public async Task<string> Get(string path, params string[] parameters) {
+            var requestUri = $"{restServerBaseAddress}{path}";
+            requestUri = AddParamsToRequestUri(requestUri, parameters);
+            return await client.GetStringAsync(requestUri);
+        }
+
+        private static string AddParamsToRequestUri(string requestUri, IReadOnlyList<string> parameters) {
+            if(parameters.Count > 0) {
+                requestUri += "?";
+                for(var i = 0; i < parameters.Count; i++) {
+                    requestUri += parameters[i];
+                    if(i < parameters.Count - 1) {
+                        requestUri += "&";
+                    }
+                }
+            }
+            return requestUri;
         }
     }
 }
