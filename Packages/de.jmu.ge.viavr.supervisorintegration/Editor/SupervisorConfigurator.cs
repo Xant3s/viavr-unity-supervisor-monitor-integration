c975509e77b90062c7c6c6459f79df15a71f2e28
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2023-04-24 16:25:46 +0200

    Copy BuildSettings to resource folder on build

---
+++ Packages/de.jmu.ge.viavr.supervisorintegration/Editor/SupervisorConfigurator.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using de.jmu.ge.viavr.UnityBridge.Core;
+
+
+namespace de.jmu.ge.viavr.supervisorintegration.editor {
+    public class SupervisorConfigurator : PackageConfigurator {
+        public override void Init() {
+            EnsureDirectory("Assets/Settings");
+            EnsureDirectory("Assets/Resources");
+            if(!File.Exists("Assets/Settings/BuildSettings.json")) return;
+            File.Copy("Assets/Settings/BuildSettings.json", "Assets/Resources/BuildSettings.json", true);
+        }
+
+        private void EnsureDirectory(string path) {
+            if(!Directory.Exists(path)) Directory.CreateDirectory(path);
+        }
+    }
+}
