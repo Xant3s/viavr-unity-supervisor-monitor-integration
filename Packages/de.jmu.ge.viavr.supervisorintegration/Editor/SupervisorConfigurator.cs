d52ccbb0aaf18dcbe44524d4c1280882e84fe28b
Fabian Maier <fabian.maier1@stud-mail.uni-wuerzburg.de>
2023-06-14 16:04:40 +0200

    supervisorIntegration.prefab now gets Added when a UnityBridge call starts

--- Packages/de.jmu.ge.viavr.supervisorintegration/Editor/SupervisorConfigurator.cs
+++ Packages/de.jmu.ge.viavr.supervisorintegration/Editor/SupervisorConfigurator.cs
@@ -1,5 +1,8 @@
 ﻿using System.IO;
 using de.jmu.ge.viavr.UnityBridge.Core;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 namespace de.jmu.ge.viavr.supervisorintegration.editor {
@@ -9,10 +12,22 @@ namespace de.jmu.ge.viavr.supervisorintegration.editor {
             EnsureDirectory("Assets/Resources");
             if(!File.Exists("Assets/Settings/BuildSettings.json")) return;
             File.Copy("Assets/Settings/BuildSettings.json", "Assets/Resources/BuildSettings.json", true);
+            InstantiateSupervisorManagerPrefab();
         }
 
         private void EnsureDirectory(string path) {
             if(!Directory.Exists(path)) Directory.CreateDirectory(path);
+        }
+
+        private void InstantiateSupervisorManagerPrefab()
+        {
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Packages/de.jmu.ge.viavr.supervisorintegration/Prefabs/Supervisor Manager.prefab");
+            if (prefab == null) return;
+            for (int i = 0; i < SceneManager.sceneCount; i++)
+            {
+                var scene = SceneManager.GetSceneAt(i);
+                PrefabUtility.InstantiatePrefab(prefab, scene);
+            }
         }
     }
 }
