96f4673cb80c20c1d8d0d6963b41000b82f4d733
Samuel Truman <samuel.truman@uni-wuerzburg.de>
2024-02-26 15:55:08 +0100

    Removed obsolete samples

--- Assets/Samples/VIA-VR Supervisor Monitor Integration/0.1.0-preview1/Stream Example/Rotate.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace de.jmu.ge.viavr.supervisorintegration.streamsample {
-    public class Rotate : MonoBehaviour {
-        [SerializeField] private float speed = 50;
-
-        private void Update() => transform.Rotate(Vector3.up, speed * Time.deltaTime);
-    }
-}
